b04b0bee3e1a8cea7a5fde6600fa50b92ba323eb
SValentyn <planet.sv01@gmail.com>
2021-04-10 10:58:44 +0300

    Fixed profiles for deployment on local and hosting

--- src/main/java/com/growingnetwork/configuration/ApplicationConfiguration.java
+++ src/main/java/com/growingnetwork/configuration/ApplicationConfiguration.java
@@ -1,12 +1,11 @@
 package com.growingnetwork.configuration;
 
-import com.amazonaws.auth.PropertiesFileCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.growingnetwork.util.SmartCopyBeanUtilsBean;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,34 +16,31 @@ import static org.modelmapper.convention.MatchingStrategies.STRICT;
 @Configuration
 public class ApplicationConfiguration {
     
-//    @Value("${amazonProperties.credentials.path}")
-//    private String s3CredentialsPath;
-    
     @Bean
     public BCryptPasswordEncoder bcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
     
     /**
-     * Getting client with default credentials (from file: /user/.aws/config) 
+     * With credentials from environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
      */
     @Bean
     public AmazonS3Client amazonS3Client() {
         return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withRegion(Regions.EU_WEST_2)
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
                 .build();
     }
     
     /**
-     * With credentials from specified file
+     * Getting client with default credentials (from file: /user/.aws/config)
      */
 //    @Bean
 //    public AmazonS3Client amazonS3Client() {
 //        return (AmazonS3Client) AmazonS3ClientBuilder
 //                .standard()
 //                .withRegion(Regions.EU_WEST_2)
-//                .withCredentials(new PropertiesFileCredentialsProvider(s3CredentialsPath))
 //                .build();
 //    }
     
--- src/main/java/com/growingnetwork/util/EmailHandler.java
+++ src/main/java/com/growingnetwork/util/EmailHandler.java
@@ -1,6 +1,7 @@
 package com.growingnetwork.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -9,8 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailHandler {
     
-//    private static final String DOMAIN_NAME = "http://ec2-18-132-120-237.eu-west-2.compute.amazonaws.com:8080";
-    private static final String DOMAIN_NAME = "http://localhost:3000";
+    @Value("${spring.profiles.active}")
+    private static String profile;
+    
+    private static final String DOMAIN_NAME = ("prod".equals(profile)) ? "http://ec2-18-132-245-35.eu-west-2.compute.amazonaws.com" : "http://localhost:8080";
     private static final String CHANGE_PASSWORD_URL = DOMAIN_NAME + "/change_password/";
     private static final String SIGN_UP_CONFIRMATION_URL = DOMAIN_NAME + "/email/confirm/";
     private static final String SIGN_UP_LETTER_SUBJECT = "Congratulations! A new account has been registered at GrowingNetwork!";
